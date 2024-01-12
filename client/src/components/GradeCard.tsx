828f2edfb7badeaf06a16a5a364d9a093890dcd7
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-01-12 16:40:50 -0300

    feat: create de grade card component

--- client/src/components/GradeCard.tsx
+++ client/src/components/GradeCard.tsx
@@ -1,1 +1,20 @@
-export {}
+import React from "react";
+import moment from "moment";
+import { IGrades } from "../interfaces/IGrades";
+
+interface GradeCardProps {
+  grade: IGrades;
+}
+
+const GradeCard: React.FC<GradeCardProps> = ({ grade }) => {
+  return (
+    <>
+      <p>{ grade.disciplina }</p>
+      <p>{ grade.bimestre }</p>
+      <p>Nota: { grade.nota }</p>
+      <p>{ moment(grade.criadoEm).format('DD/MM/YYYY') }</p>
+    </>
+  )
+}
+
+export default GradeCard;
--- client/src/pages/Grades.tsx
+++ client/src/pages/Grades.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { gradesList } from "../services/apiRequests";
 import { IGrades } from "../interfaces/IGrades";
+import GradeCard from "../components/GradeCard";
 
 export default function Grades() {
   const [grades, setGrades] = useState([]);
@@ -22,14 +23,7 @@ export default function Grades() {
       <div>
         <h2>LISTA DE NOTAS</h2>
         { grades.map((grade: IGrades) => (
-          <span key={ grade.id }>
-            <p>{ grade.id }</p>
-            <p>{ grade.disciplina }</p>
-            <p>{ grade.bimestre }</p>
-            <p>{ grade.nota }</p>
-            <p>{ grade.criadoEm }</p>
-            <p>{ grade.atualizadoEm }</p>
-          </span>
+          <GradeCard grade={ grade } key={ grade.id } />
         ))}
       </div>
     </>
